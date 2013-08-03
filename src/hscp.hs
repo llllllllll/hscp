25e949b27cf8bdccaa02c0fc83e358582c24938d
Joe Jevnik <joejev@gmail.com>
2013-08-03 00:21:49 -0400

    Added regex ignores instead of exact string matching.

--- src/hscp.hs
+++ src/hscp.hs
@@ -10,51 +10,82 @@ import System.IO
 import Control.Concurrent
 import Control.Monad
 import Control.Applicative
+import Text.Regex.Base.RegexLike
+import Text.Regex.TDFA
+import Foreign.C.Types
 
+data PollNode = PollNode { file_name    :: FilePath
+                         , is_directory :: Bool
+                         , edit_time    :: Foreign.C.Types.CTime
+                         }
+
+
+mk_poll_node :: (FilePath,Bool,Foreign.C.Types.CTime) -> PollNode
+mk_poll_node (p,b,e) = PollNode p b e
 
 main :: IO ()
 main = do
     args <- getArgs
-    hscp_poll (head args)
+    hscp_start_polling (head args)
 
-
-hscp_poll config_path = do
+hscp_start_polling :: String -> IO ()
+hscp_start_polling config_path = do
     config <- lines <$> readFile config_path
     let user_name = drop 5 (head config)
         pass = drop 9 (config!!1)
         host = drop 5 (config!!2)
         dir = drop 10 (config!!3)
         clone_dir = drop 15 (config!!4)
-        poll_int = drop 13 (config!!5)
-        ignored = (tail . drop 6) config
-    cs <- filter (`notElem` ignored) <$> getDirectoryContents dir
-    pid <- getProcessID
+        poll_int = read $ drop 13 (config!!5)
+        ignored = drop 7 config :: [String]
+    cs <- filter (\c -> not $ any (\i -> i =~ c :: Bool) ignored) 
+          <$> getDirectoryContents dir
     setCurrentDirectory dir
     ts <- mapM getFileStatus cs
-    let access_times = zip3 cs (map isDirectory ts) (map accessTime ts)
-    hscp_poll' user_name pass host 
-               dir clone_dir (read poll_int) ignored access_times
-                       
-        
-hscp_poll' user_name pass host dir clone_dir poll_int ignored access_times = do
-    setCurrentDirectory dir
-    cs <- filter (`notElem` ignored) <$> getDirectoryContents dir
-    ts <- mapM getFileStatus cs
-    let access_times' = zip3 cs (map isDirectory ts) (map accessTime ts)
-    mapM_ (\((a,b,c),(a',b',c')) -> 
-               if c /= c' then if b 
-                               then putStrLn ("Change found on dir " ++ a 
-                                              ++ ", pushing!") >>
-                               system ("scp -r " ++ a ++ " " 
-                                       ++ user_name ++ "@" ++ host ++ ":" 
-                                       ++ clone_dir ++ a) >> return ()
-                               else putStrLn ("Change found on file " ++ a
-                                              ++ ", pushing!") >>
-                               system ("scp -r " ++ a ++ " " 
-                                       ++ user_name ++ "@" ++ host ++ ":" 
-                                       ++ clone_dir ++ a) >> return ()
-               else putStrLn $ "No edits found on " ++ a ++ " this poll!") 
-              $ zip access_times access_times'
+    let polls = map mk_poll_node $ 
+                zip3 cs (map isDirectory ts) (map accessTime ts)
+    mapM_ (\p -> if is_directory p 
+                 then let a = file_name p in
+                      putStrLn ("Innitial push on  dir " ++ a 
+                                ++ ", pushing!") >>
+                      system ("scp -r " ++ a ++ " " 
+                              ++ user_name ++ "@" ++ host ++ ":" 
+                              ++ clone_dir ++ a) >> return ()
+                 else let a = file_name p in
+                      putStrLn ("Innitial push on file " ++ a
+                                ++ ", pushing!") >>
+                      system ("scp -r " ++ a ++ " " 
+                              ++ user_name ++ "@" ++ host ++ ":" 
+                              ++ clone_dir ++ a) >> return ()) polls
     putStrLn "polling..."
     threadDelay poll_int
-    hscp_poll' user_name pass host dir clone_dir poll_int ignored access_times'
+    hscp_poll user_name pass host dir clone_dir poll_int ignored polls
+                           
+hscp_poll user_name pass host dir clone_dir poll_int ignored polls = do
+    setCurrentDirectory dir
+    cs <- filter (\c -> not $ any (\i -> i =~ c :: Bool) ignored) 
+          <$> getDirectoryContents dir
+    ts <- mapM getFileStatus cs
+    let polls' = map mk_poll_node 
+                 $ zip3 cs (map isDirectory ts) (map accessTime ts)
+    mapM_ (\(p,p') -> 
+               if edit_time p /= edit_time p' then 
+                   if is_directory p 
+                   then let a = file_name p in
+                        putStrLn ("Change found on dir " ++ a 
+                                  ++ ", pushing!") >>
+                        system ("scp -r " ++ a ++ " " 
+                                ++ user_name ++ "@" ++ host ++ ":" 
+                                ++ clone_dir ++ a) >> return ()
+                   else let a = file_name p in
+                        putStrLn ("Change found on file " ++ a
+                                  ++ ", pushing!") >>
+                        system ("scp -r " ++ a ++ " " 
+                                ++ user_name ++ "@" ++ host ++ ":" 
+                                ++ clone_dir ++ a) >> return ()
+               else putStrLn $ "No edits found on " ++ file_name p 
+                        ++ " this poll!") 
+              $ zip polls polls'
+    putStrLn "polling..."
+    threadDelay poll_int
+    hscp_poll user_name pass host dir clone_dir poll_int ignored polls'
