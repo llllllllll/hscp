9408114e667efb63bfb5454dd092abf9063bbcf2
Joe Jevnik <joejev@gmail.com>
2013-08-04 23:40:10 -0400

    md5 hasing with LazyByteStrings to only push when a file is changed.

--- src/hscp.hs
+++ src/hscp.hs
@@ -4,7 +4,6 @@ import System.Environment
 import System.Directory
 import System.Process
 import System.FilePath
-import System.Posix.Files
 import System.Posix.Process
 import System.Exit
 import System.IO
@@ -13,42 +12,43 @@ import Control.Monad
 import Control.Applicative
 import Text.Regex.Base.RegexLike
 import Text.Regex.TDFA
-import Foreign.C.Types
 import Data.Maybe
 import Data.List
+import Data.Digest.Pure.MD5
+import qualified Data.ByteString.Lazy as B
 
 -- Data to represent a file being polled.
-data PollNode = PollNode { file_name    :: FilePath
-                         , edit_time    :: Foreign.C.Types.CTime
-                         }
+data PollNode = PollNode { file_name :: FilePath
+                         , file_hash :: MD5Digest
+                         } deriving Show
 
 -- Equality of PollNodes is based only on file_name.
 instance Eq PollNode where
     (==) a b = file_name a == file_name b
     (/=) a b = not $ a == b
 
--- Creates a PollNode with the name, isDirectory, and the modification time.
-mk_poll_node :: (FilePath,Foreign.C.Types.CTime) -> PollNode
+-- Creates a PollNode with the name and a hash.
+mk_poll_node :: (FilePath,MD5Digest) -> PollNode
 mk_poll_node (p,e) = PollNode p e
 
--- Parses args and begins pollin.
+-- Parses args and begins polling.
 main :: IO ()
 main = do
     args <- getArgs
     config <- (parse_config . lines) <$> readFile (head args)
     hscp_start_polling config
 
--- Starts the polling process.
+-- Starts the polling process. 
 hscp_start_polling :: (String,String,String,String,String,Int,[String]) -> IO ()
 hscp_start_polling (user_name,pass,host,dir,clone_dir,poll_int,ignored) = do
     cs <- recurs_dir_conts dir ignored
-    ts <- mapM getFileStatus cs
+    ts <- mapM B.readFile cs
     setCurrentDirectory dir
-    let polls = map mk_poll_node $ zip cs (map modificationTime ts)
-    putStrLn "Innitial push!"
+    let polls = map mk_poll_node $ zip cs (map md5 ts)
+    putStrLn "Initial push!"
     system ("scp -r " ++ dir ++ " " ++ user_name ++ '@':host ++ 
             ':':clone_dir) >> return ()
-    putStrLn "polling..."
+    putStrLn "Starting to poll..."
     threadDelay poll_int
     hscp_poll (user_name,pass,host,dir,clone_dir,poll_int,ignored,polls)
 
@@ -59,34 +59,32 @@ hscp_poll (user_name,pass,host,dir,clone_dir,poll_int,ignored,polls) = do
     setCurrentDirectory dir
     cs <- recurs_dir_conts dir ignored
     polls' <- get_new_polls cs polls
-    mapM_ (\(p,p') -> print (file_name p,edit_time p,edit_time p')) polls' -- Testing
     mapM_ (attempt_scp_push user_name host clone_dir) $ polls'
-    putStrLn "polling..."
     threadDelay poll_int
     hscp_poll (user_name,pass,host,dir,clone_dir,poll_int,ignored,map fst polls')
 
--- Parses the config files lines into their fields.
+-- Parses the config files lines into their fields. t
 parse_config :: [String] -> (String,String,String,String,String,Int,[String])
-parse_config config = ( drop 5 (head config)                        -- user_name
-                      , drop 9 (config!!1)                          -- pass
-                      , drop 5 (config!!2)                          -- host
-                      , drop 10 (config!!3)                         -- dir
-                      , drop 15 (config!!4)                         -- clone_dir
-                      , read $ drop 13 (config!!5)                  -- poll_int
-                      , "\\.":"\\.\\.":(drop 7 config :: [String])) -- ignored
+parse_config config = ( drop 5 (head config)                           -- user_name
+                      , drop 9 (config!!1)                             -- pass
+                      , drop 5 (config!!2)                             -- host
+                      , drop 10 (config!!3)                            -- dir
+                      , drop 15 (config!!4)                            -- clone_dir
+                      , read $ drop 13 (config!!5)                     -- poll_int
+                      , "^\\.$":"^\\.\\.$":(drop 7 config :: [String]))-- ignored
 
 -- Gets a list of [New PollNode,Old PollNode) to be used to check the times.
 get_new_polls :: [FilePath] -> [PollNode] -> IO [(PollNode,PollNode)]
 get_new_polls cs polls = do
-    ts <- mapM getFileStatus cs
-    let polls' = map mk_poll_node $ zip cs (map modificationTime ts)
-    return [(p,fromMaybe (mk_poll_node (file_name p,0)) (find (==p) polls))
-                | p <- polls'] 
+    ts <- mapM B.readFile cs
+    let polls' = map mk_poll_node $ zip cs (map md5 ts)
+    return [(p,fromMaybe (mk_poll_node (file_name p,md5 B.empty)) (find (==p) polls))
+                | p <- polls']
 
 -- Gets the contents of dir that do not match any of the regex in ignored.
 get_filtered_contents :: [String] -> FilePath -> IO [FilePath]
 get_filtered_contents ignored dir = 
-    filter (\c -> not $ any (\i -> i =~ c :: Bool) ignored) 
+    filter (\c -> not $ any (\i -> c =~ i :: Bool) ignored) 
                <$> getDirectoryContents dir
 
 -- Gets the recursive contents of dir that do not match any of the 
@@ -106,7 +104,7 @@ recurs_dir_conts dir ignored = do
 -- are different.
 attempt_scp_push :: String -> String -> FilePath -> (PollNode,PollNode) -> IO ()
 attempt_scp_push user_name host clone_dir (p,p') = 
-    if edit_time p /= edit_time p'
+    if file_hash p /= file_hash p' -- Need to fix.
     then let a = file_name p in
          putStrLn ("Change found on file " ++ a ++ ", pushing!") >>
          system ("scp " ++ a ++ " " ++ user_name ++ '@':host ++ 
